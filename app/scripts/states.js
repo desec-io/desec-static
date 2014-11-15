78b3921ecf00e410398d776df849e8988e4c9ca7
Nils Wisiol <info@nils-wisiol.de>
2014-11-15 18:15:54 -0500

    feat(states): introduced app states (ui router)

---
+++ app/scripts/states.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Configure the states of the application.
+ */
+angular.module('desecClientApp').config(function($stateProvider, $urlRouterProvider) {
+
+	// For any unmatched url, redirect to /
+	$urlRouterProvider.otherwise("/");
+
+	// Now set up the states
+	$stateProvider
+		.state('homepage', {
+			url: "/",
+			templateUrl: "views/homepage.html"
+		})
+		.state('dashboard', {
+			url: "/dashboard",
+			templateUrl: "views/dashboard.html"
+		})
+		.state('dashboard.domainadd', {
+			url: "/dashboard/add",
+			templateUrl: "views/domainadd.html"
+		})
+		.state('dashboard.domaintest', {
+			url: "/dashboard/test",
+			templateUrl: "views/domaintest.html"
+		})
+		.state('dashboard.domainlist', {
+			url: "/dashboard/list",
+			templateUrl: "views/domainlist.html"
+		})
+});
