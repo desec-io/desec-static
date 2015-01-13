9bc104315a702ab018dd2203d668656be0a70d1f
Nils Wisiol <info@nils-wisiol.de>
2015-01-13 12:44:06 +0100

    feat(): introduced translation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,23 @@
  *
  * Main module of the application.
  */
-angular
-  .module('desecClientApp', [
+angular.module('desecClientApp', [
 		//'ui.router',
-	]);
+		'pascalprecht.translate',
+	])
+
+angular.module('desecClientApp').config(function($translateProvider) {
+		
+		$translateProvider.preferredLanguage('en');
+
+		$translateProvider.translations('en', {
+			'TITLE': 'Hello',
+			'FOO': 'This is a paragraph'
+		});
+
+		$translateProvider.translations('de', {
+			'TITLE': 'Hallo',
+			'FOO': 'Dies ist ein Paragraph'
+		});
+		
+	})
