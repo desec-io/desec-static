a54cea7a1c2221d6617e9b513a8efc01f43a52cf
Nils Wisiol <info@nils-wisiol.de>
2015-01-13 18:29:55 +0100

    feat(): re-introduced state manager and introduced translation service
    URLs now contains the language as first "folder", i.e.
    - /de/products and
    - /en/products
    
    On /, the preferred language is determined and then the user is forwarded
    to, let's say, /de/ for the i18nized homepage.
    
    All URLs are in the state manager's hostname#/de/products fashion.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,22 +9,6 @@
  * Main module of the application.
  */
 angular.module('desecClientApp', [
-		//'ui.router',
+		'ui.router',
 		'pascalprecht.translate',
 	])
-
-angular.module('desecClientApp').config(function($translateProvider) {
-		
-		$translateProvider.preferredLanguage('en');
-
-		$translateProvider.translations('en', {
-			'TITLE': 'Hello',
-			'FOO': 'This is a paragraph'
-		});
-
-		$translateProvider.translations('de', {
-			'TITLE': 'Hallo',
-			'FOO': 'Dies ist ein Paragraph'
-		});
-		
-	})
--- app/scripts/states.js
+++ app/scripts/states.js
@@ -3,16 +3,66 @@
 /**
  * Configure the states of the application.
  */
-angular.module('desecClientApp').config(function() {
+angular.module('desecClientApp').config(function ($urlRouterProvider, $stateProvider, $translateProvider) {
 
-//	// For any unmatched url, redirect to /
-//	$urlRouterProvider.otherwise("/");
-//
-//	// Now set up the states
-//	$stateProvider
-//		.state('homepage', {
-//			url: "/",
-//			templateUrl: "views/homepage.html"
-//		})
+	// available languages, first one is preferred
+	var languages = ['en', 'de'];
 
+	// preferred language
+	$translateProvider
+		.registerAvailableLanguageKeys(['en', 'de'], {
+			'en_US': 'en',
+			'en_UK': 'en',
+			'en_AU': 'en',
+			'en_NZ': 'en',
+			'de_DE': 'de',
+			'de_CH': 'de',
+			'de_AT': 'de'
+		})
+		.determinePreferredLanguage();
+
+	// set up translations
+	angular.forEach(languages, function (lang) {
+		$translateProvider.translations(lang, {
+			'TITLE': 'This is ' + lang,
+			'FOO': 'This is a <strong>' + lang + '</strong> paragraph'
+		});
+	});
+
+	// Set up i18n root state
+	$stateProvider.state('i18n', {
+		template: '<ui-view/>',
+		url: '/:lang',
+		abstract: true,
+		controller: function($translate, $stateParams) {
+			$translate.use($stateParams.lang);
+		}
+	})
+	
+	$stateProvider
+		
+		.state('root', {
+			url: '/',
+			controller: function($translate, $state) {
+				$state.go('i18n.home', {lang: $translate.preferredLanguage()});
+			}
+		})
+
+		.state('i18n.home', {
+			url: "/",
+			templateUrl: "views/homepage.html"
+		})
+		
+		.state('i18n.product', {
+			url: '/product',
+			template: '<ui-view/>'
+		})
+	
+		.state('i18n.product.dyndns', {
+			url: "/dyndns",
+			templateUrl: "views/products/dyndns.html"
+		})
+
+	$urlRouterProvider.otherwise('/');
+	
 });
