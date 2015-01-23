5f4a0882d9d55c2eb9e6b5b0427e39c684783c91
Peter Thomassen <peter@desec.io>
2015-01-23 10:19:57 +0100

    feat(i18n): few more texts; have Grunt watch text JSON files

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,8 @@ module.exports = function (grunt) {
 				files: [
 					'<%= yeoman.app %>/{,*/}*.html',
 					'.tmp/styles/{,*/}*.css',
-					'<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+					'<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+					'<%= yeoman.app %>/texts/*.json'
 				]
 			}
 		},
