41228b934060d82a48483fc3ad664676586c49e5
Peter Thomassen <peter@desec.io>
2015-06-19 13:03:18 +0200

    fix(): clean up landing page JS

--- app/scripts/landing2.js
+++ app/scripts/landing2.js
@@ -6,8 +6,8 @@
 $(window).load(function() {
 	$('#spinner').fadeOut();
 	$('#preloader').delay(300).fadeOut('slow');
-	setTimeout(function(){$('.first-slide div:first-child').addClass('fadeInDown');},100);
-	setTimeout(function(){$('.first-slide div:last-child').addClass('fadeInRight');},100);
+//	setTimeout(function(){$('.first-slide div:first-child').addClass('fadeInDown');},100);
+//	setTimeout(function(){$('.first-slide div:last-child').addClass('fadeInRight');},100);
 	setTimeout(function(){$('.color-switcher').addClass('slideInLeft');},100);
 });
 
@@ -20,57 +20,6 @@ $(document).ready(function(e) {
 	$('.main-navi .has-dropdown a').click(function(){
 		$(this).parent().find('.dropdown').toggleClass('expanded');
 	});
-	
-	////////////////////////////////////////////////////////////
-	//INTERNAL ANCHOR LINKS SCROLLING (NAVIGATION)
-	$(".scroll").click(function(event){		
-		event.preventDefault();
-		$('html, body').animate({scrollTop:$(this.hash).offset().top-80}, 1000, 'easeInOutQuart');
-	});
-	
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 500) {
-			$('#scroll-top').addClass('visible');
-		} else {
-			$('#scroll-top').removeClass('visible');
-		}
-	});
-	
-	//SCROLL-SPY
-	// Cache selectors
-	var lastId,
-		topMenu = $(".main-navi"),
-		topMenuHeight = topMenu.outerHeight(),
-		// All list items
-		menuItems = topMenu.find("a"),
-		// Anchors corresponding to menu items
-		scrollItems = menuItems.map(angular.noop);
-	
-	// Bind to scroll
-	$(window).scroll(function(){
-	   // Get container scroll position
-	   var fromTop = $(this).scrollTop()+topMenuHeight+200;
-	   
-	   // Get id of current scroll item
-	   var cur = scrollItems.map(function(){
-		 if ($(this).offset().top < fromTop)
-		   return this;
-	   });
-	   // Get the id of the current element
-	   cur = cur[cur.length-1];
-	   var id = cur && cur.length ? cur[0].id : "";
-	   
-	   if (lastId !== id) {
-		   lastId = id;
-		   // Set/remove active class
-		   menuItems
-			 .parent().removeClass("active")
-			 .end().filter("[href=#"+id+"]").parent().addClass("active");
-	   }
-	});
-	////////////////////////////////////////////////////////////////////
-	
-	
 
 });/*/Document ready*/
 
