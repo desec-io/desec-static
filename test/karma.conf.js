5be230a6499dd239b3fee75b4f895f24fa5b9a94
Peter Thomassen <peter@desec.io>
2016-01-05 11:14:24 +0100

    fix(): dubious fixes

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function (config) {
 			'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
 			'bower_components/angular-resource/angular-resource.js',
 			'bower_components/angular-sanitize/angular-sanitize.js',
+			'bower_components/angular-animate/angular-animate.js',
 			'bower_components/angular-mailchimp/angular-mailchimp.js',
 			'bower_components/angulartics/src/angulartics.js',
 			'bower_components/angulartics/src/angulartics-ga.js',
